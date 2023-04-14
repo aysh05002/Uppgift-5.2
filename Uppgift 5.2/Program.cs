099244f97833cbccdd1496a433ddee42711f539e
Admin <Admin@TEB123008>
2023-04-14 08:49:24 +0200

    Add project files.

---
+++ Uppgift 5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hur många heltal vill du skriva in?");
+        int heltal = Convert.ToInt32(Console.ReadLine());
+
+        string[] tal = new string[heltal];
+
+        Console.WriteLine($"Skriv in {heltal} heltal:");
+        for(int i = 0; i < tal.Length; i++)
+        {
+            tal[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Här är talen du skrev in:");
+        for(int i = 0; i < tal.Length; i++)
+        {
+            Console.WriteLine($"Tal {i+1}: {tal[i]}");
+        }
+    }
+}
